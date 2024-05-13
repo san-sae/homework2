0df204b5201501a169c9f3045861688e5e429ea8
choiyoo <choi.yoo.lim.12@gmail.com>
2024-05-13 16:38:06 +0900

    수정

--- smalloc.h
+++ smalloc.h
@@ -3,7 +3,7 @@ struct _smheader {
 	size_t size ; // 헤더 다음에 오는 데이터 영역의 길이
 	uint8_t used ; // 사용 여부를 나타내는 단일 바이트 속성
 	struct _smheader * next ; // 다음 데이터 헤더를 가리키는 포인터
-} ;
+};
 
 typedef struct _smheader 	smheader ;
 typedef struct _smheader *	smheader_ptr ;
