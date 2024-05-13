687fd3b2fab1a5abe815d3b5ab1b5a17a06ae039
san-sae <97453758+san-sae@users.noreply.github.com>
2024-05-13 16:57:08 +0900

    Update test1.c

--- test1.c
+++ test1.c
@@ -6,15 +6,15 @@ int main ()
 {
 	void *p1, *p2, *p3, *p4 ;
 
-	//smdump() ;
+	smdump() ;
 
 	p1 = smalloc(2000) ; 
 	printf("smalloc(2000):%p\n", p1) ; 
-	//smdump() ;
+	smdump() ;
 
 	p2 = smalloc(2500) ; 
 	printf("smalloc(2500):%p\n", p2) ; 
-	//smdump() ;
+	smdump() ;
 
 	//sfree(p1) ; 
 	//printf("sfree(%p)\n", p1) ; 
@@ -22,9 +22,9 @@ int main ()
 
 	p3 = smalloc(1000) ; 
 	printf("smalloc(1000):%p\n", p3) ; 
-	//smdump() ;
+	smdump() ;
 
 	p4 = smalloc(1000) ; 
 	printf("smalloc(1000):%p\n", p4) ; 
-	//smdump() ;
+	smdump() ;
 }
